17adc99ac8c2fccc92b351d3849ebc50ed22b43d
Евгений Гурин <e.a.gurin@gmail.com>
2020-06-11 01:33:06 +0300

    update3

--- homework.py
+++ homework.py
@@ -11,12 +11,14 @@ class Calculator:
 
     def get_today_stats(self):
         today = dt.date.today()
-        return sum(record.amount for record in self.records if record.date == today)
+        return sum(record.amount for record in self.records
+                    if record.date == today)
 
     def get_week_stats(self):
         today = dt.date.today()
         week_ago = today - dt.timedelta(days=7)
-        return sum(record.amount for record in self.records if week_ago <= record.date <= today)
+        return sum(record.amount for record in self.records
+                    if week_ago <= record.date <= today)
 
     def get_balance(self):
         balance = self.limit - self.get_today_stats()
@@ -34,23 +36,24 @@ class CashCalculator(Calculator):
                     'usd': [self.USD_RATE, 'USD'],
                     'rub': [self.RUB_RATE, 'руб']
                 }
-        
-        if self.get_balance() == 0:
+        get_balance = self.get_balance()
+        if get_balance == 0:
             return 'Денег нет, держись'
         if currency not in currency_dict:
             raise ValueError('Калькулятор не поддерживает данную валюту.')
         currency_rate, currency_name = currency_dict[currency]
         balance_cash = round((self.get_balance()) / currency_rate, 2)
-        if self.get_balance() > 0:
+        if get_balance > 0:
             return f'На сегодня осталось {balance_cash} {currency_name}'
-        return f'Денег нет, держись: твой долг - {abs(balance_cash)} ' \
-                f'{currency_name}'
+        return (f'Денег нет, держись: твой долг - {abs(balance_cash)} ' \
+                f'{currency_name}')
+
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         if self.get_balance() > 0:
-            return 'Сегодня можно съесть что-нибудь ещё, но с общей ' \
-                   f'калорийностью не более {self.get_balance()} кКал'
+            return ('Сегодня можно съесть что-нибудь ещё, но с общей ' \
+                    f'калорийностью не более {self.get_balance()} кКал')
         return 'Хватит есть!'
 
 
